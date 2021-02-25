1bc29af8785d2170bf2f7b4e66ae1eb72fe65151
Azmain Adel <azmainadel47@gmail.com>
2021-02-25 15:31:40 +0600

    Time: 32 ms (63.45%), Space: 14.1 MB (82.16%) - LeetHub

---
+++ powx-n/powx-n.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        def function(base = x, exponent = abs(n)):
+            if exponent == 0:
+                return 1
+            elif exponent % 2 == 0:
+                return function(base * base, exponent // 2)
+            else:
+                return base * function(base * base, (exponent - 1) // 2)
+
+        result = function()
+
+        return float(result) if n >= 0 else 1 / result
