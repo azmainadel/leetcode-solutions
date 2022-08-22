9831179c9a8f041f6609707534cc194a371a6203
Azmain Adel <azmainadel47@gmail.com>
2022-08-22 14:48:03 +0600

    Time: 30 ms (95.90%), Space: 14 MB (26.29%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,16 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        openParentheses = ['(', '{', '[']
-        closeParentheses = [')', '}', ']']
+        p_dict = {")": "(", "]": "[", "}": "{"}
         
         for c in s:
-            if c in openParentheses:
+            if c not in p_dict:
                 stack.append(c)
-            else:
-                if len(stack) != 0 and stack[-1] == openParentheses[closeParentheses.index(c)]:
-                    stack.pop()
-                else:
-                    return False
-        return len(stack) == 0
+                continue
+            if not stack or stack[-1] != p_dict[c]:
+                return False
+            stack.pop()
+                
+        return not stack
     
