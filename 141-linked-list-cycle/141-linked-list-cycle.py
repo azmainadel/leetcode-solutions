6e2873109af35b9a561c583d87594bbbc91c1d19
Azmain Adel <azmainadel47@gmail.com>
2022-08-23 12:28:03 +0600

    Time: 106 ms (21.58%), Space: 17.5 MB (67.21%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -6,11 +6,11 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        tortoise, hare = head, head
+        hare, tortoise = head, head
         
         while hare and hare.next:
-            tortoise = tortoise.next
             hare = hare.next.next
+            tortoise = tortoise.next
             
             if hare == tortoise:
                 return True
