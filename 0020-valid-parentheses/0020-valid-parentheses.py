666b1ee389552d041771e76723b56e2bdfe22dea
Azmain Adel <azmainadel47@gmail.com>
2022-10-15 14:05:08 +0600

    Time: 59 ms (40.22%), Space: 13.9 MB (26.20%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        p_dict = {")": "(", "]": "[", "}": "{"}
+        
+        for c in s:
+            if c not in p_dict:
+                stack.append(c)
+                continue
+            
+            if not stack or stack[-1] != p_dict[c]:
+                return False
+            
+            stack.pop()
+
+        return not stack
+        
